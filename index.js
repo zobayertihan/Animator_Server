5b89c60f35dbadcd93e7914f4d468065573ae6c1
zobayertihan <zobayertihan@gmail.com>
2022-11-09 01:23:35 +0600

    getting services/id

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
-
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -12,6 +10,37 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 app.use(cors());
 app.use(express.json());
+
+
+async function run() {
+    try {
+        const serviceCollection = client.db('Animator').collection('services');
+        const reviewCollection = client.db('Animator').collection('reviews');
+
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            // limit(3) 
+            res.send(services);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(e => console.error(e));
+
 
 app.get('/', (req, res) => {
     res.send('Animator Server Running')
