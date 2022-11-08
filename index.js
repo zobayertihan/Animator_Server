e189ba116c54f20eeb06d1d82dd7775af0a8d3e6
zobayertihan <zobayertihan@gmail.com>
2022-11-08 23:22:04 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ilc8iz8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Animator Server Running')
+})
+
+app.listen(port, () => {
+    console.log(`Animator Loading on port: ${port}`);
+})
